1630a5a43b3e4f8b9eb3ab8e14e043c734fbbd95
eliezermuller <eliezerm@balink.net>
2023-05-08 15:16:15 +0300

    update version

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,33 +1,26 @@
 
 import './App.css';
-import {Component} from'react'
-
+import { Component } from 'react'
 import Dynamic_rendersComp from './Demo3_DynamicRendering/Dynamic_renders'
-
-import Ex7_q1Comp  from './Demo8_Forms/Ex7_q1'
+import Ex7_q1Comp from './Demo8_Forms/Ex7_q1'
 import Demo6Comp from './Demo6_REST_API/Demo6'
 import PropsComp from './Demo1_BasicComponentAndProps/PropsTest'
 
-class App extends Component
+class App extends Component {
+  constructor() {
+    super();
+  }
 
-{
-    constructor()
-    
-    {
-      super();
-    }
-    
-    render()
-    {
-         return (
-           <div> <h1>Hello from App</h1>
+  render() {
+    return (
+      <div> <h1>Hello from App</h1>
 
-       <Dynamic_rendersComp/>
-          
-           </div>
-         )
+        <Demo6Comp />
 
-    }
+      </div>
+    )
+
+  }
 }
 
 export default App;
